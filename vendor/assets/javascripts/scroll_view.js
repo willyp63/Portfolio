d03b5a5c059c3633f338fb10716c1d5dc6457dfe
Wil Pirino <wpirino1@gmail.com>
2016-09-18 18:14:53 -0700

    option keyword removed

--- vendor/assets/javascripts/scroll_view.js
+++ vendor/assets/javascripts/scroll_view.js
@@ -1,6 +1,6 @@
 $.fn.scrollView = function (options, complete) {
     defaultOptions = {duration: 1000, offset: 0};
-    for (let optionName in options) { defaultOptions[optionName] = options[optionName]; }
+    for (var optionName in options) { defaultOptions[optionName] = options[optionName]; }
     return this.each(function () {
         $('html, body').stop().animate({
             scrollTop: $(this).offset().top + defaultOptions.offset
